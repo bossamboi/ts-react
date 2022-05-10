b8df9a19034405c37282e8dd4b94103c7554b5b9
Eric Sohng <ericsohng@gmail.com>
2022-05-09 17:24:44 -0700

    complete refactoring to TS

---
+++ boxes/src/Box.tsx
@@ -0,0 +1,50 @@
+interface IBoxProps {
+  id: string;
+  width: number;
+  height: number;
+  backgroundColor: string;
+  remove(id: string): void;
+}
+
+/** Colored box presentation
+ *
+ * Props:
+ * - id (unique)
+ * - width
+ * - height
+ * - backgroundColor
+ * - remove (function to call)
+ *
+ * BoxList -> Box
+ */
+
+function Box({
+  id,
+  width = 5,
+  height = 5,
+  backgroundColor,
+  remove,
+}: IBoxProps) {
+  /** Remove a box. */
+  function handleRemove(): void {
+    remove(id);
+  }
+
+  return (
+    <div className="Box">
+      <div
+        className="Box-box"
+        style={{
+          height: `${height}em`,
+          width: `${width}em`,
+          backgroundColor: backgroundColor,
+        }}
+      />
+      <button className="Box-removeBtn" onClick={handleRemove}>
+        Remove The Box!
+      </button>
+    </div>
+  );
+}
+
+export default Box;
